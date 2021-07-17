5842bcff2b3789f1e13d005eef1f2ce2df2e9328
kapil <kapil@neostencil.com>
2021-07-17 11:07:12 +0530

    add project

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent  {
+
+ public num1:number;
+ public num2:number;
+ public result:number;
+
+ addition(){
+   this.result=this.num1+this.num2;
+ }
+ subs()
+ {
+  this.result=this.num1-this.num2;
+ }
+ multi()
+ {
+  this.result=this.num1*this.num2;
+ }
+ division()
+ {
+  this.result=this.num1/this.num2;
+ }
+
+}
